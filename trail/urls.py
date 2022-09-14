77162f38976d6f3bb29894ca74a448f4d5dc906d
CathalShandon <Cathalshandon@gmail.com>
2022-09-14 18:22:49 +0000

    make migrations

--- trail/urls.py
+++ trail/urls.py
@@ -5,11 +5,11 @@ Imports
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.Home, name='home'),
     path("aboutus/", views.AboutUs, name="aboutus"),
     path('trail/', views.PostList.as_view(), name="trail"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post_detail', views.star_ratings, name="star_ratings"),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
