948defe6a50c98c7742ff2a732984e9ad3fb9cba
CathalShandon <Cathalshandon@gmail.com>
2022-09-15 10:07:13 +0000

    add urls for create edit and delte post

--- trail/urls.py
+++ trail/urls.py
@@ -2,14 +2,16 @@
 Imports
 # 3rd party:
 """
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('', views.Home, name='home'),
     path("aboutus/", views.AboutUs, name="aboutus"),
     path('trail/', views.PostList.as_view(), name="trail"),
+    path('create/', views.create_post, name='create_post'),
+    path('edit_post/<slug:slug>', views.edit_post, name='post_edit'),
+    path('delete_post/<slug:slug>', views.delete_post, name='post_delete'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('post_detail', views.star_ratings, name="star_ratings"),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
