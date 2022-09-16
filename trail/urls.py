b96062d9964e96abb778ccd71f5b72727bd9f68b
CathalShandon <Cathalshandon@gmail.com>
2022-09-16 15:02:53 +0000

    add profile urls

--- trail/urls.py
+++ trail/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path("aboutus/", views.AboutUs, name="aboutus"),
     path('trail/', views.PostList.as_view(), name="trail"),
+     path('profile/', views.ProfileList.as_view(), name='profile'),
+    path('edit_profile/', views.edit_profile, name='profile_edit'),
+    path(
+      'profile/<str:username>',
+      views.ProfileDetail.as_view(),
+      name='profile_detail'),
     path('create/', views.create_post, name='create_post'),
     path('edit_post/<slug:slug>', views.edit_post, name='post_edit'),
     path('delete_post/<slug:slug>', views.delete_post, name='post_delete'),
